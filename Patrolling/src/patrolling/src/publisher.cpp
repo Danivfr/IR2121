fd794b5a9850deaa4007105775687b89e276251c
Danivfr <al435552@uji.es>
2025-11-12 16:35:02 +0100

    patrolling v1.2

--- Patrolling/src/patrolling/src/publisher.cpp
+++ Patrolling/src/patrolling/src/publisher.cpp
@@ -8,9 +8,29 @@ int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv); 
     auto node = rclcpp::Node::make_shared("publisher");
-    auto publisher
+    auto publisher = node->create_publisher<geometry_msgs::msg::PoseStamped>("goal_pose", 10);
+    geometry_msgs::msg::PoseStamped message;
+    rclcpp::WallRate loop_rate(500ms);
 
 
+    while (rclcpp::ok()) {
 
+    message.header.stamp = node->now();
+    message.header.frame_id = "map";
+    message.pose.position.x = 4.0;
+    message.pose.position.y = 3.0;
+    message.pose.position.z = 0.0;
+    message.pose.orientation.x = 0.0;
+    message.pose.orientation.y = 0.0;
+    message.pose.orientation.z = 0.0;
+    message.pose.orientation.w = 1.0;
+
+    publisher->publish(message);
+    rclcpp::spin_some(node);
+    loop_rate.sleep();
+    }
+
+    rclcpp::shutdown();
+    return 0;
 
 }
